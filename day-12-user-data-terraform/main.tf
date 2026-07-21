a25289806af8968b6936e5f06c9a1840aac03cae
shivamani3286-cloud <shivamani3286@gmail.com>
2026-07-21 13:07:17 +0530

    day-12

---
+++ day-12-user-data-terraform/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "name" {
+    ami = "ami-01edba92f9036f76e"
+    instance_type = "t3.micro"
+    tags = {
+      name = "shiva"
+    }
+    user_data = file("userdata.sh")
+  
+}
